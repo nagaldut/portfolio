8af5decba81d8886060d5ab4b0dab0aec1136d11
NehaRani Agalduty <125945418+nagaldut@users.noreply.github.com>
2024-11-28 21:33:47 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+const main = {
+  name: 'Neha Rani Agalduty',
+  mail: 'Neharani1727@gmail.com',
+  img: 'user.png',
+  role: ['Data Scientist'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/nagaldut'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/nehaagaldutyrani/'
+    },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'neharani1727@@gmail.com'}?Subject=Hello%20again`
+    }
+  ],
+  links: [
+    { name: 'Projects', link: 'https://github.com/nagaldut' },
+    { name: 'Blogs', link: 'https://medium.com/@neha' },
+    { name: 'Resume', link: 'https://drive.google.com/file/d/1GqQslKwhWtEsjdyRv3FMWQaG3bAq7Mf3/view?usp=sharing'},
+  ]
+};
